667b62887b6ecda80163cc78d90943360783a8d4
Daniel Tamm <daniel@notima.se>
2025-04-17 14:17:16 +0200

    "Fix" WS provider to Apache CXF.

--- src/main/java/com/svea/webpayadmin/WebpayAdminBase.java
+++ src/main/java/com/svea/webpayadmin/WebpayAdminBase.java
@@ -35,7 +35,7 @@ public class WebpayAdminBase {
 		existingSpi = System.getProperty("javax.xml.ws.spi.Provider");
 		existingSoapFactory = System.getProperty("javax.xml.soap.SOAPFactory");
 
-//		System.setProperty("javax.xml.ws.spi.Provider","org.apache.cxf.jaxws.spi.ProviderImpl");
+		System.setProperty("javax.xml.ws.spi.Provider","org.apache.cxf.jaxws.spi.ProviderImpl");
 //		System.setProperty("javax.xml.soap.SOAPFactory", "com.sun.xml.messaging.saaj.soap.SAAJMetaFactoryImpl");
 		
 		// Read logging properties to remove warnings from 
